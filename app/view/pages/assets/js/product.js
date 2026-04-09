9eac26d848dfe5971cf3bac31877c300aa27af1e
Athos Cordeiro Rosa <athoscordeiro@hotmail.com>
2026-04-08 21:47:15 -0400

    C
    C

--- app/view/pages/assets/js/product.js
+++ app/view/pages/assets/js/product.js
@@ -1,62 +1,76 @@
+import { SellingPriceCalculator } from "../components/SellingPriceCalculator.js";
+
+//import { SellingPriceCalculator } from "../components/SellingPriceCalculator.js";
 const InsertButton = document.getElementById('insert');
-const Action = document.getElementById('action')
-const Id = document.getElementById('id')
-const totalTax = document.getElementById('total_imposto');
+const form = document.getElementById('form');
+const Action = document.getElementById('action');
+const Id = document.getElementById('id');
+const inputTotalTax = document.getElementById('total_imposto');
+const inputProfitMargin = document.getElementById('margem_lucro');
+const inputOperatingCost = document.getElementById('custo_operacional');
+const inputPurchasePrice = document.getElementById('preco_compra');
 
+Inputmask("currency", {
+    radixPoint: ',',
+    inputtype: "text",
+    prefix: 'R$',
+    autoGroup: true,
+    groupSeparator: '.',
+    rightAlign: false,
+    onBeforeMask: function (value) {
+        return String(value).replace('.', ',');
+    }
+}).mask("#preco_venda, #preco_compra");
+Inputmask("currency", {
+    radixPoint: ',',
+    inputtype: "text",
+    prefix: '%',
+    autoGroup: true,
+    groupSeparator: '.',
+    rightAlign: false,
+    onBeforeMask: function (value) {
+        return String(value).replace('.', ',');
+    }
+}).mask("#total_imposto, #margem_lucro, #custo_operacional");
 
-$(document).ready(function () {
-    $("#preco_venda").maskMoney({
-        prefix: 'R$ ',
-        allowNegative: true,
-        thousands: '.',
-        decimal: ',',
-        affixesStay: true
-    });
+function determineSalePrice() {
+    const purchasePrice = parseFloat(String(inputPurchasePrice.value).replace('R$', '').replace('.', '').replace(',', '.')) || 0;
+    const tax = parseFloat(String(inputTotalTax.value).replace('%', '').replace(',', '.')) || 0;
+    const profitMargin = parseFloat(String(inputProfitMargin.value).replace('%', '').replace(',', '.')) || 0;
+    const operatingCost = parseFloat(String(inputOperatingCost.value).replace('%', '').replace(',', '.')) || 0;
+    if (profitMargin <= 0 && purchasePrice <= 0) {
+        document.getElementById('resultado-row').className = 'resultado-row mb-2 d-none';
+        return;
+    }
+    const result = SellingPriceCalculator.create()
+        .addTotalTax(tax)
+        .addProfitMargin(profitMargin)
+        .addOperatingCost(operatingCost)
+        .addPurchasePrice(purchasePrice)
+        .getData();
+    document.getElementById('val-venda').innerHTML = `${result.valor_venda_sugerido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById('val-margem').innerHTML = `${result.valor_margem_lucro.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById('val-custo').innerHTML = `${result.valor_custo_operacional.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById('val-imposto').innerHTML = `${result.valor_total_imposto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`;
+    document.getElementById('resultado-row').className = 'resultado-row mb-2';
+}
+
+inputTotalTax.addEventListener('input', () => {
+    determineSalePrice();
 });
 
-$(document).ready(function () {
-    $("#preco_compra").maskMoney({
-        prefix: 'R$ ',
-        allowNegative: true,
-        thousands: '.',
-        decimal: ',',
-        affixesStay: true
-    });
-});
-$(document).ready(function () {
-    $("#margem_lucro").maskMoney({
-        prefix: '% ',
-        allowNegative: true,
-        thousands: '.',
-        decimal: ',',
-        affixesStay: true
-    });
+inputProfitMargin.addEventListener('input', () => {
+    determineSalePrice();
 });
 
-$(document).ready(function () {
-    $("#custo_operacional").maskMoney({
-        prefix: '% ',
-        allowNegative: true,
-        thousands: '.',
-        decimal: ',',
-        affixesStay: true
-    });
-    
+inputOperatingCost.addEventListener('input', () => {
+    determineSalePrice();
+});
+inputPurchasePrice.addEventListener('input', () => {
+    determineSalePrice();
 });
 
-$(document).ready(function () {
-    $("#imposto").maskMoney({
-        prefix: '% ',
-        allowNegative: true,
-        thousands: '.',
-        decimal: ',',
-        affixesStay: true
-    });
-    
-});
-
-
-
+//  CARREGA DADOS DE EDIÇÃO (se existirem)
 (async () => {
     const editData = await api.temp.get('product:edit');
     if (editData) {
@@ -66,7 +80,6 @@ $(document).ready(function () {
         // Preenche todos os campos pelo atributo name
         for (const [key, value] of Object.entries(editData)) {
             const field = form.querySelector(`[name="${key}"]`);
-
             if (!field) continue;
 
             if (field.type === 'checkbox') {
@@ -76,51 +89,60 @@ $(document).ready(function () {
             }
         }
     } else {
+        // Modo cadastro novo
         Action.value = 'c';
         Id.value = '';
     }
 })();
 
 
-InsertButton.addEventListener('click', async () => {
-    let timer = 2000;
-    $('#insert').prop('disabled', true);
+InsertButton.addEventListener("click", async () => {
+  let timer = 3000;
+  $("#insert").prop("disabled", true);
+  const formData = new FormData(form);
+  const data = {};
+  for (const [key, value] of formData.entries()) {
+    data[key] = value;
+  }
+  data.preco_venda = String(data.preco_venda)
+    .replace("R$", "")
+    .replace(".", "")
+    .replace(",", ".");
+  data.preco_compra = String(data.preco_compra)
+    .replace("R$", "")
+    .replace(".", "")
+    .replace(",", ".");
+  data.total_imposto = String(data.total_imposto)
+    .replace("%", "")
+    .replace(",", ".");
+  data.margem_lucro = String(data.margem_lucro)
+    .replace("%", "")
+    .replace(",", ".");
+  data.custo_operacional = String(data.custo_operacional)
+    .replace("%", "")
+    .replace(",", ".");
+  // Se NÃO é cadastro novo, pega o ID para update, senão deixa null para insert
 
-    const precoVendaLimpo = $("#preco_venda").maskMoney('unmasked')[0];
-    const precoCompraLimpo = $("#preco_compra").maskMoney('unmasked')[0];
-    const margemLucroLimpo = $("#margem_lucro").maskMoney('unmasked')[0];
-    const custoOperacionalLimpo = $("#custo_operacional").maskMoney('unmasked')[0];
-    const impostoLimpo = $("#imposto").maskMoney('unmasked')[0];
+  let id = Action.value !== "c" ? Id.value : null;
 
+  try {
+    const response =
+      Action.value === "c"
+        ? await api.product.insert(data)
+        : await api.product.update(id, data);
 
-    const data = formToJson(form);
-
-    data.preco_venda = precoVendaLimpo;
-    data.preco_compra = precoCompraLimpo;
-    data.margem_lucro = margemLucroLimpo;
-    data.custo_operacional = custoOperacionalLimpo;
-    data.imposto = impostoLimpo;
-
-    let id = Action.value !== 'c' ? Id.value : null;
-
-    try {
-        const response = Action.value === 'c'
-            ? await api.product.insert(data)
-            : await api.product.update(id, data);
-
-        if (!response.status) {
-            toast('error', 'Erro', response.msg, timer);
-            return;
-        }
-        toast('success', 'Sucesso', response.msg, timer);
-        form.reset();
-        setTimeout(() => {
-            api.window.close();
-        }, timer);
-
-    } catch (err) {
-        toast('error', 'Falha', 'Erro: ' + err.message, timer);
-    } finally {
-        $('#insert').prop('disabled', false);
+    if (!response.status) {
+      toast("error", "Erro", response.msg, timer);
+      return;
     }
+    toast("success", "Sucesso", response.msg, timer);
+    form.reset();
+    setTimeout(() => {
+      api.window.close();
+    }, timer);
+  } catch (err) {
+    toast("error", "Falha", "Erro: " + err.message, timer);
+  } finally {
+    $("#insert").prop("disabled", false);
+  }
 });
